d0183a359c3853bb43f418880a7c2ef6a875ae4e
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-25 01:30:09 +0200

    change fontawsome icon with Emoji

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ for (let i = 0; i < hoursArray.length; i++) {
   let row = `<div class="row">
   <div class="col-2 time-block hour">${hour}</div>
   <textarea class="col-8">${getLocalStorage}</textarea>
-  <button class="col-2 saveBtn" data-hour="${hour}">
-    <i class="fa fa-save" aria-hidden="true"></i>
-  </button>
+  <button class="col-2 saveBtn" data-hour="${hour}">💾</button>
 </div>`;
   console.log(hour);
   containerEl.append(row);
@@ -26,5 +24,6 @@ $("button").on("click", (event) => {
   let key = $(event.target).attr("data-hour");
   let value = $(event.target).siblings("textarea").val();
   localStorage.setItem(key, value);
+  console.log(value);
   
 });
