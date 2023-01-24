439b0ffb0da5742d18681ad94169d74c510b1e20
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-25 00:53:19 +0200

    create button event listener on click to set localStorage form the input

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ let containerEl = $(".container");
 
 let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+
 for (let i = 0; i < hoursArray.length; i++) {
   const hour = hoursArray[i];
   let row = `<div class="row">
   <div class="col-2 time-block hour">${hour}</div>
   <textarea class="col-8"></textarea>
-  <button class="col-2 saveBtn">
+  <button class="col-2 saveBtn" data-hour="${hour}">
     <i class="fa fa-save" aria-hidden="true"></i>
   </button>
 </div>`;
@@ -18,4 +19,11 @@ for (let i = 0; i < hoursArray.length; i++) {
   
 }
 
-
+$("button").on("click", (event) => {
+  console.log(event);
+  console.log(event.target);
+  let key = $(event.target).attr("data-hour");
+  let value = $(event.target).siblings("textarea").val();
+  localStorage.setItem(key, value);
+  
+});
