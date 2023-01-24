a1e5b841e0ff025f8ff912600e59df0a986e1759
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-25 01:01:00 +0200

    get local storage if is set before

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (let i = 0; i < hoursArray.length; i++) {
   const hour = hoursArray[i];
+  let getLocalStorage = localStorage.getItem(hour) || "";
   let row = `<div class="row">
   <div class="col-2 time-block hour">${hour}</div>
-  <textarea class="col-8"></textarea>
+  <textarea class="col-8">${getLocalStorage}</textarea>
   <button class="col-2 saveBtn" data-hour="${hour}">
     <i class="fa fa-save" aria-hidden="true"></i>
   </button>
