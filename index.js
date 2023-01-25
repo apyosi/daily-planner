07b71f0325be007bf5ee4573644379c4cc725923
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-25 02:03:09 +0200

    Create past, present future logic

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 let $currentDayEl = $("#currentDay");
-$currentDayEl.text(moment().format("dddd, MMMM Do"));
+$currentDayEl.text(moment().format("dddd, MMMM Do hh:mm:ss"));
 let containerEl = $(".container");
 
 let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
+let currentHour = parseInt((moment().format("h")));
+console.log(currentHour);
+console.log(typeof(currentHour));
 
 for (let i = 0; i < hoursArray.length; i++) {
   const hour = hoursArray[i];
+  let cssClass = "";
+  if ( hour > currentHour) {
+    cssClass = "future";
+  } else if ( hour < currentHour ) {
+    cssClass = "past";
+  } else  {
+    cssClass = "present";
+  }
+  console.log(cssClass);
   let getLocalStorage = localStorage.getItem(hour) || "";
   let row = `<div class="row">
   <div class="col-2 time-block hour">${hour}</div>
-  <textarea class="col-8">${getLocalStorage}</textarea>
+  <textarea class="col-8 ${cssClass}">${getLocalStorage}</textarea>
   <button class="col-2 saveBtn" data-hour="${hour}">💾</button>
 </div>`;
   console.log(hour);
