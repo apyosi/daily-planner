0d4d4a781fbdda9ad7c95f53c1fd8e86f32f43f4
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-25 02:49:19 +0200

    Add js comments and small code changes

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
+//Variables declaration
 let $currentDayEl = $("#currentDay");
-$currentDayEl.text(moment().format("dddd, MMMM Do hh:mm:ss"));
+$currentDayEl.text(moment().format("dddd, MMMM Do"));
 let containerEl = $(".container");
-
 let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-let currentHour = parseInt((moment().format("h")));
-console.log(currentHour);
-console.log(typeof(currentHour));
+let hoursDisplay = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
+let currentHour = parseInt(moment().format("h"));
 
+//Loop to create the html for the business hours
 for (let i = 0; i < hoursArray.length; i++) {
   const hour = hoursArray[i];
+  const hourDisplay = hoursDisplay[i];
   let cssClass = "";
-  if ( hour > currentHour) {
+  if (hour > currentHour) {
     cssClass = "future";
-  } else if ( hour < currentHour ) {
+  } else if (hour < currentHour) {
     cssClass = "past";
-  } else  {
+  } else {
     cssClass = "present";
   }
-  console.log(cssClass);
-  let getLocalStorage = localStorage.getItem(hour) || "";
+  let getLocalStorage = localStorage.getItem(hour) || ""; //get the Local Storage if already set
   let row = `<div class="row">
-  <div class="col-2 time-block hour">${hour}</div>
+  <div class="col-2 time-block hour">${hourDisplay}</div>
   <textarea class="col-8 ${cssClass}">${getLocalStorage}</textarea>
   <button class="col-2 saveBtn" data-hour="${hour}">💾</button>
-</div>`;
-  console.log(hour);
+  </div>`;
   containerEl.append(row);
-  
 }
 
+// Event listener onClick to save and set the Local Storage item
 $("button").on("click", (event) => {
-  console.log(event);
-  console.log(event.target);
   let key = $(event.target).attr("data-hour");
   let value = $(event.target).siblings("textarea").val();
   localStorage.setItem(key, value);
-  console.log(value);
-  
 });
